879f297be2087663981f304deeb6c2a0bc80b94e
JohnnyShwarts <104720406+JohnnyShwarts@users.noreply.github.com>
2022-06-18 21:04:40 +0300

    HW_Git Bash

---
+++ HW.sh
@@ -0,0 +1,8 @@
+#!/bin/bash              
+cd 2_p                                                              
+mkdir 1_pp 2_pp 3_pp
+cd 1_pp/
+touch 1.txt 2.txt 3.txt 4.json 5.json
+mkdir 1_1 2_2 3_3
+ls -la
+mv C:/Users/Екатерина/Desktop/bashTest/2_p/1_pp/1.txt C:/Users/Екатерина/Desktop/bashTest/2_p/1_pp/2.txt C:/Users/Екатерина/Desktop/bashTest/2_p/1_pp/3_3
